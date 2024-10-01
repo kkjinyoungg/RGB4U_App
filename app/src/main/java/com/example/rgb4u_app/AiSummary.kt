3c653800288409d1566ed627ca7670a78eefdc34
2yoona <thisisprofita@gmail.com>
2024-10-01 11:09:32 +0900

    이거 아님

--- app/src/main/java/com/example/rgb4u_app/AiSummary.kt
+++ app/src/main/java/com/example/rgb4u_app/AiSummary.kt
@@ -13,11 +13,7 @@ class AiSummary {
 
     private val firebaseDatabase: FirebaseDatabase = FirebaseDatabase.getInstance()
     private val client = OkHttpClient()
-<<<<<<< HEAD
-    private val apiKey = "API키 넣기"  // API 키 설정
-=======
-    private val apiKey = "API키 여기에"  // API 키 설정
->>>>>>> origin/backend
+    private val apiKey = ""  // API 키 설정
     private val TAG = "AiSummary" // Logging Tag
 
     // 특정 diaryId의 situation과 thoughts를 가져와 ChatGPT API로 분석 후 저장하는 함수
