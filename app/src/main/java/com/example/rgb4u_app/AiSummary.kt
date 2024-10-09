2729582b05fae0ec71583a5088f1be56328e477e
2yoona <thisisprofita@gmail.com>
2024-10-09 16:05:03 +0900

    json 변경

--- app/src/main/java/com/example/rgb4u_app/AiSummary.kt
+++ app/src/main/java/com/example/rgb4u_app/AiSummary.kt
@@ -15,7 +15,7 @@ class AiSummary {
 
     private val firebaseDatabase: FirebaseDatabase = FirebaseDatabase.getInstance()
     private val client = OkHttpClient()
-    private val apiKey = ""  // API 키 설정
+    private val apiKey = ""  // API 키 설정 (따옴표 안에 키 넣기)
     private val TAG = "AiSummary" // Logging Tag
 
     // 특정 diaryId의 situation과 thoughts를 가져와 ChatGPT API로 분석 후 저장하는 함수
