9e1d8ed6094128728e2fbba146641656f68523d5
jinyoung <etesoleil@naver.com>
2024-11-20 17:27:11 +0900

    11.20 - 캘린더&통계 날짜 스피너 추가 반영(마이페이지&회원가입은 순환만 막음), 찐비밀번호 입력 화면 추가(연결X)

--- app/src/main/java/com/example/rgb4u_app/ui/activity/login/LoginActivity.kt
+++ app/src/main/java/com/example/rgb4u_app/ui/activity/login/LoginActivity.kt
@@ -8,17 +8,17 @@ import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.example.rgb4u_app.R
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.GoogleAuthProvider
-import com.google.firebase.auth.FirebaseUser
+import com.example.rgb4u_app.ui.activity.home.MainActivity
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
 import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.database.FirebaseDatabase
-import com.example.rgb4u_app.ui.activity.home.MainActivity
 
 class LoginActivity : AppCompatActivity() {
 
