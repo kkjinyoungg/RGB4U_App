7f4a88ec268ae074226730b92077aec02f6f2948
2yoona <thisisprofita@gmail.com>
2024-11-20 19:17:02 +0900

    캘린더 디테일 화면 1 완료 (달라진하루 버튼은 조정해야)

--- app/src/main/java/com/example/rgb4u_app/ui/activity/calendar/CalenderDetailActivity.kt
+++ app/src/main/java/com/example/rgb4u_app/ui/activity/calendar/CalenderDetailActivity.kt
@@ -10,6 +10,7 @@ import android.widget.TextView
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat
 import com.example.rgb4u_app.R
 import com.example.rgb4u_appclass.DiaryViewModel
 import com.google.android.material.button.MaterialButton
@@ -19,11 +20,37 @@ import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import com.google.android.material.chip.Chip
+import com.google.android.material.chip.ChipGroup
 
 class CalenderDetailActivity : AppCompatActivity() {
 
     private lateinit var database: DatabaseReference
     private val diaryViewModel: DiaryViewModel by viewModels()
+
+    // 감정 타입별 상위 카테고리 매핑
+    private val emotionCategoryMap = mapOf(
+        "움찔하는" to "Surprise", "황당한" to "Surprise", "깜짝 놀란" to "Surprise",
+        "어안이 벙벙한" to "Surprise", "아찔한" to "Surprise", "충격적인" to "Surprise",
+        "걱정스러운" to "Fear", "긴장된" to "Fear", "불안한" to "Fear",
+        "겁나는" to "Fear", "무서운" to "Fear", "암담한" to "Fear",
+        "기운 없는" to "Sadness", "서운한" to "Sadness", "슬픈" to "Sadness",
+        "눈물이 나는" to "Sadness", "우울한" to "Sadness", "비참한" to "Sadness",
+        "약 오른" to "Anger", "짜증나는" to "Anger", "화난" to "Anger",
+        "억울한" to "Anger", "분한" to "Anger", "끓어오르는" to "Anger",
+        "정 떨어지는" to "Disgust", "불쾌한" to "Disgust", "싫은" to "Disgust",
+        "모욕적인" to "Disgust", "못마땅한" to "Disgust", "미운" to "Disgust"
+    )
+
+    // Chip 배경색 가져오기 함수
+    private fun getChipColor(category: String) = when (category) {
+        "Surprise" -> getColorStateList(R.color.surpriseColor_dark)
+        "Fear" -> getColorStateList(R.color.fearColor_dark)
+        "Sadness" -> getColorStateList(R.color.sadnessColor_dark)
+        "Anger" -> getColorStateList(R.color.angerColor_dark)
+        "Disgust" -> getColorStateList(R.color.disgustColor_dark)
+        else -> getColorStateList(R.color.defaultChipColor) // 기본 색상
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -55,8 +82,11 @@ class CalenderDetailActivity : AppCompatActivity() {
         val situationTextView = findViewById<TextView>(R.id.situationTextView)
         val thoughtTextView = findViewById<TextView>(R.id.thoughtTextView)
         val emotionIntensityTextView = findViewById<TextView>(R.id.emotionIntensityTextView)
-        val emotionTypeTextView = findViewById<TextView>(R.id.emotionTypeTextView)
         val emotionIntensityImageView = findViewById<ImageView>(R.id.emotionIntensityImageView)
+        val emotionIntensityTextView02 = findViewById<TextView>(R.id.emotionIntensityTextView02)
+        // 칩 그룹 참조
+        val selectedChipGroup = findViewById<ChipGroup>(R.id.SummarySelectedChipGroup)
+        val emotionChipGroup = findViewById<ChipGroup>(R.id.SummaryEmotionChipGroup)
 
         // Firebase로부터 데이터 가져오기
         if (userId != null && datefordb != null) {
@@ -89,10 +119,62 @@ class CalenderDetailActivity : AppCompatActivity() {
 
                                 // TextView에 감정 강도 설정
                                 emotionIntensityTextView.text = "${emotionDegreeInt + 1}단계"
-                                emotionTypeTextView.text = emotionDegreeString
+                                emotionIntensityTextView02.text = emotionDegreeString
 
                                 // 감정 강도에 따른 이미지 설정
                                 emotionIntensityImageView.setImageResource(getEmotionImageResource(emotionDegreeImage))
+
+                                // emotionTypes는 리스트 형태로 가져온다
+                                val emotionTypesList = userInputSnapshot.child("emotionTypes").children.mapNotNull { it.getValue(String::class.java) }
+                                val emotionTypes = emotionTypesList.joinToString(", ") // 리스트를 문자열로 변환
+
+                                // AI 분석 감정용 emotion 리스트 가져오기 ()
+                                val emotionsList = dataSnapshot.child("emotion").children.mapNotNull { it.getValue(String::class.java) }
+
+                                // 감정 강도와 감정 종류를 로그에 출력
+                                Log.d("SummaryMainActivity", "감정 강도: $emotionDegreeInt ($emotionDegreeString), 감정 종류: $emotionTypes")
+
+                                //selectedChipGroup에 감정 추가
+                                for (emotion in emotionTypesList) {
+                                    val category = emotionCategoryMap[emotion] ?: "default" // 감정의 상위 카테고리 찾기
+                                    val chip = Chip(this@CalenderDetailActivity).apply {
+                                        text = emotion
+                                        isCloseIconVisible = false // 닫기 아이콘 숨기기
+                                        isClickable = false // 칩 클릭 비활성화
+                                        isFocusable = false // 포커스 비활성화
+
+                                        shapeAppearanceModel = shapeAppearanceModel.toBuilder()
+                                            .setAllCornerSizes(50f) // 모서리 둥글기
+                                            .build()
+
+                                        setTextColor(ContextCompat.getColor(this@CalenderDetailActivity, R.color.white)) // 텍스트 색상
+                                        chipBackgroundColor = getChipColor(category) // 배경색 설정
+                                    }
+                                    // 선택한 감정 추가
+                                    selectedChipGroup.addView(chip)
+                                }
+
+                                for (emotion in emotionsList) {
+                                    val chip = layoutInflater.inflate(R.layout.summary_single_chip, emotionChipGroup, false) as Chip
+                                    chip.text = emotion
+                                    chip.isCloseIconVisible = false // 닫기 아이콘 숨기기
+                                    chip.isClickable = false // 칩 클릭 비활성화
+                                    chip.isFocusable = false // 포커스 비활성화
+
+                                    // Chip의 shapeAppearanceModel 설정
+                                    chip.shapeAppearanceModel = chip.shapeAppearanceModel.toBuilder()
+                                        .setAllCornerSizes(50f) // 모서리 둥글기
+                                        .build()
+
+                                    // 텍스트 색상 설정
+                                    chip.setTextColor(ContextCompat.getColor(this@CalenderDetailActivity, R.color.black)) // 텍스트 색상
+                                    // 칩 테두리 색상 설정
+                                    chip.setChipStrokeColorResource(R.color.black) // 테두리 색상
+                                    chip.chipStrokeWidth = 2f // 테두리 두께 설정 (필요시 조정)
+
+                                    emotionChipGroup.addView(chip) // ChipGroup에 추가
+                                }
+
                             }
 
                             override fun onCancelled(databaseError: DatabaseError) {
